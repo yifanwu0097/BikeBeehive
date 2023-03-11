e95d5f06e7b75154e83d2915651815f7d8be4058
yuanjundu <118921121+CodePie1@users.noreply.github.com>
2023-03-11 20:57:27 +0000

    remove cronitor

--- DataScrapper/weatherdata.py
+++ DataScrapper/weatherdata.py
@@ -1,24 +1,12 @@
 import traceback
-import cronitor
-cronitor.api_key = 'c2d3a43ef9c7406c8dfb624ce09fc256'
 from weatherScrapper import weatherScrapper
 
 # run forever...
 
-cronitor.Monitor.put(
-    key='background-weather-scrapper',
-    type='job',
-    schedule='*/30 * * * *'
-)
-
 while True:
     try:
         # weatherForeScrapper()
-        monitor = cronitor.Monitor('background-weather-scrapper')
         weatherScrapper()
-        monitor.ping(state='run')
-        monitor.ping(state='complete')
-        monitor.ping(state='fail')
 
     except:
         # if there is any problem, print the traceback
