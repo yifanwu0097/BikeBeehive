252d2ff220e7446c0814d907813e70573e619c8e
yifanwu <117227528+yifanwu0097@users.noreply.github.com>
2023-03-12 22:21:28 +0000

    Update weatherdata.py

--- DataScrapper/weatherdata.py
+++ DataScrapper/weatherdata.py
@@ -3,12 +3,11 @@ from weatherScrapper import weatherScrapper
 
 # run forever...
 
-while True:
-    try:
-        # weatherForeScrapper()
-        weatherScrapper()
-        print("Finished populating current weather data.")
+try:
+    # weatherForeScrapper()
+    weatherScrapper()
+    print("Finished populating current weather data.")
 
-    except:
-        # if there is any problem, print the traceback
-        print(traceback.format_exc())
+except:
+    # if there is any problem, print the traceback
+    print(traceback.format_exc())
