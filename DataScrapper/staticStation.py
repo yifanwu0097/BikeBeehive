a8cda86f065207fc313e00569aa21c0a1ee36c95
CodePie1 <yuanjundu9911@gmail.com>
2023-04-09 21:58:08 +0100

    add markers

--- DataScrapper/staticStation.py
+++ DataScrapper/staticStation.py
@@ -2,10 +2,11 @@ import json
 import pymysql.cursors
 from createdbtable import cursor, connection
 
+
 def staticStation():
     # STATIC DATA
     # Read dublin.json and get data store in dub
-    f = open('dublin.json', 'r')
+    f = open('DataScrapper\dublin.json', 'r')
     content = f.read()
     dub = json.loads(content)
 
@@ -18,7 +19,10 @@ def staticStation():
                   float(dbstation.get('latitude')),
                   float(dbstation.get('longitude')))
         # Then populate static data into table dublin_station
-        sql = """INSERT INTO db_bikes.dublin_station VALUES(%i,"%s","%s",%f,%f);"""% dbvals
+        sql = """INSERT INTO db_bikes.dublin_station VALUES(%i,"%s","%s",%f,%f);""" % dbvals
         cursor.execute(sql)
         connection.commit()
         print("Finished populating static station data.")
+
+
+staticStation()
--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, jsonify, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
 import pymysql.cursors
@@ -19,54 +19,78 @@ app.config['CACHE_DEFAULT_TIMEOUT'] = 300
 # db = SQLAlchemy(app)
 cache = Cache(app)
 
+
 @app.route("/")
 @cache.cached()
 def index():
     return render_template('index.html')
 
 # Display all stations info
+
+
 @app.route("/stations")
 @cache.cached()
 def stations():
-    conn = pymysql.connect(host=URI, user=USER, password=PASSWORD, port=PORT, database=DB)
+    conn = pymysql.connect(host=URI, user=USER,
+                           password=PASSWORD, port=PORT, database=DB)
     with conn.cursor() as cursor:
-        sql = """
-        SELECT s.number, s.name, s.address, s.position_lat, s.position_lng, a.available_bikes, a.available_bike_stands, MAX(a.last_update)
-        FROM db_bikes.station_info as s
-        JOIN db_bikes.station_new_availability as a
-        ON s.number = a.number
-        GROUP BY s.name, time
-        ORDER BY s.name, time;
-        """
-        cursor.execute(sql)
-        stations_data = cursor.fetchall()
+        # select and show all of the stations without showing its occupancy
+        # sql_static = """
 
-    return json.dumps(stations_data)
+        #     """
+        # cursor.execute(sql_static)
+
+        sql = """
+            SELECT d.number, d.name, d.address, d.position_lat, d.position_lng,a.available_bikes, a.available_bike_stands, a.last_update
+            FROM (
+                SELECT * FROM db_bikes.station_info LIMIT 114
+            ) as d
+            JOIN db_bikes.station_new_availability as a
+            ON d.number = a.number
+            WHERE (a.number, a.last_update) 
+            IN (SELECT number, MAX(last_update) FROM db_bikes.station_new_availability 
+            GROUP BY number)
+            ORDER BY number DESC;
+            """
+        cursor.execute(sql)
+        stations_data = [dict(zip([column[0] for column in cursor.description], row))
+                         for row in cursor.fetchall()]
+        print(stations_data)
+    return jsonify(stations_data)
+
 
 # Display selected station's availability
+
+
 @app.route("/availability/<int:station_id>")
 @cache.cached()
 def get_availability():
-    conn = pymysql.connect(host=URI, user=USER, password=PASSWORD, port=PORT, database=DB)
+    conn = pymysql.connect(host=URI, user=USER,
+                           password=PASSWORD, port=PORT, database=DB)
     with conn.cursor() as cursor:
         sql = """
-        SELECT s.name, a.last_update as time, a.available_bikes, a.available_bike_stands
-        FROM db_bikes.station_info as s
-        JOIN db_bikes.station_new_availability as a
-        ON s.number = a.number
-        WHERE s.number = {station_id}
-        GROUP BY s.name, time
-        ORDER BY s.name, time;
+            SELECT s.name, a.last_update, a.available_bikes, a.available_bike_stands
+            FROM db_bikes.station_info as s
+            JOIN db_bikes.station_new_availability as a
+            ON s.number = a.number
+            WHERE s.number = {station_number}
+            AND (a.number, a.last_update) 
+                        IN (SELECT number, MAX(last_update) FROM db_bikes.station_new_availability 
+                        GROUP BY number)
+            GROUP BY s.name, a.last_update
+            ORDER BY s.name, a.last_update desc;
         """
         cursor.execute(sql)
         availability_data = cursor.fetchall()
 
     return json.dumps(availability_data)
 
+
 @app.route("/hourly/<int:station_id>")
 @cache.cached()
 def get_hourly_availability():
-    conn = pymysql.connect(host=URI, user=USER, password=PASSWORD, port=PORT, database=DB)
+    conn = pymysql.connect(host=URI, user=USER,
+                           password=PASSWORD, port=PORT, database=DB)
     with conn.cursor() as cursor:
         sql = """
         SELECT s.name, a.available_bikes, a.available_bike_stands, EXTRACT(HOUR FROM last_update) as hourly
@@ -87,22 +111,11 @@ def get_hourly_availability():
 @cache.cached()
 def weather_forecast():
     WEATHER_APIKEY = "f0d90ef7fcc8781efadf746287963079"
-    WEATHER_FORE_URI = "https://api.openweathermap.org/data/2.5/onecall?" 
+    WEATHER_FORE_URI = "https://api.openweathermap.org/data/2.5/onecall?"
     response = requests.get(WEATHER_FORE_URI + "lat=53.3498" + "&lon=-6.2603"
-         + "&exclude=current,minutely,alerts" + "&appid=" + WEATHER_APIKEY).json()
+                            + "&exclude=current,minutely,alerts" + "&appid=" + WEATHER_APIKEY).json()
     # wait to be finalised
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
-
-
-
-
-
-
-
-
-    
-
-    
-    
