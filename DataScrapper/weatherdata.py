b89683ee404305e13cfaeec8d485f3071513beb8
Yifan Wu <117227528+yifanwu0097@users.noreply.github.com>
2023-05-03 12:21:56 +0200

    code refactoring
    Clean the code

--- DataScrapper/weatherdata.py
+++ DataScrapper/weatherdata.py
@@ -2,13 +2,10 @@ import traceback
 from weatherScrapper import weatherScrapper
 
 # run forever...
-
 while True:
     try:
-        # weatherForeScrapper()
         weatherScrapper()
         print("Finished populating current weather data.")
 
     except:
-        # if there is any problem, print the traceback
         print(traceback.format_exc())
